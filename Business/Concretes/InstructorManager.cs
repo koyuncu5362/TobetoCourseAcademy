3da9212d0eb0aa2f1db903ce288711866351212b
koyuncu5362 <koyuncu.m@hotmail.com>
2023-11-10 16:30:28 +0300

    Add project files.

---
+++ Business/Concretes/InstructorManager.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Concretes;
+using Entities.Concretes;
+
+namespace Business.Concretes
+{
+    public class InstructorManager
+    {
+        public void Add(Instructor instructor)
+        {
+            AdoNetInstructorDal adoNetInstructorDal= new AdoNetInstructorDal();
+            adoNetInstructorDal.Add(instructor);
+        }
+    }
+ 
+}
